de54458cb6d4a333c05333cdf2fce7803947ad43
KwakNW <68180545+KwakNW@users.noreply.github.com>
2021-11-17 17:09:13 +0900

    Create 타켓넘버.py

---
+++ Programmers/DP/타켓넘버.py
@@ -0,0 +1,16 @@
+answer = 0
+def dfs(idx, numbers, target, value):
+    global answer
+    if idx == len(numbers) and target == value:
+        answer += 1
+        return
+    elif idx == len(numbers) and target != value:
+        return
+    
+    dfs(idx+1, numbers, target, value+numbers[idx])
+    dfs(idx+1, numbers, target, value-numbers[idx])
+
+def solution(numbers, target):
+    global answer
+    dfs(0, numbers, target, 0)
+    return answer
